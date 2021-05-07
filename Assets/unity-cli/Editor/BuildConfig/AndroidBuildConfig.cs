ebb72272f1d28d73a15e9298a7cc64b329ad248a
strix <korstrix@gmail.com>
2021-05-07 23:41:12 +0900

    리팩토링

---
+++ Assets/unity-cli/Editor/BuildConfig/AndroidBuildConfig.cs
@@ -0,0 +1,89 @@
+using System;
+using UnityEngine;
+using UnityEditor;
+using System.Collections.ObjectModel;
+using static Unity_CLI.CLIBuilder;
+
+namespace Unity_CLI
+{
+    [CreateAssetMenu(fileName = "AndroidBuildConfig", menuName = Unity_CLIString.CreateAssetMenu_Prefix + "/AndroidBuildConfig")]
+    public class AndroidBuildConfig : BuildConfigBase
+    {
+        public override BuildTarget buildTarget => BuildTarget.Android;
+
+        public string keyalias_name;
+        public string keyalias_password;
+
+        /// <summary>
+        /// Keystore 파일의 경로입니다. `파일경로/파일명.keystore` 까지 쓰셔야 합니다.
+        /// <para>UnityProject/Asset/ 기준의 상대 경로입니다. </para>
+        /// <para>예를들어 UnityProject/Asset 폴더 밑에 example.keystore가 있으면 "/example.keystore" 입니다.</para>
+        /// </summary>
+        public string keystore_relativePath;
+        public string keystore_password;
+
+        /// <summary>
+        /// CPP 빌드를 할지 체크, CPP빌드는 오래 걸리므로 Test빌드가 아닌 Alpha 빌드부터 하는걸 권장
+        /// 아직 미지원
+        /// </summary>
+        public ScriptingImplementation scriptingBackEnd;
+
+        public int bundleVersionCode;
+
+        public override void ResetSetting()
+        {
+            base.ResetSetting();
+
+            BuildTargetGroup targetGroup = BuildPipeline.GetBuildTargetGroup(buildTarget);
+            scriptingBackEnd = PlayerSettings.GetScriptingBackend(targetGroup);
+            bundleVersionCode = PlayerSettings.Android.bundleVersionCode;
+        }
+
+        public override void OnPreBuild(CLIBuilder builder, ReadOnlyDictionary<string, string> commandLine)
+        {
+            PlayerSettings.Android.keyaliasName = keyalias_name;
+            PlayerSettings.Android.keyaliasPass = keyalias_password;
+
+            PlayerSettings.Android.keystoreName = Application.dataPath + keystore_relativePath;
+            PlayerSettings.Android.keystorePass = keystore_password;
+            PlayerSettings.Android.bundleVersionCode = bundleVersionCode;
+
+            // if (usecppbuild)
+            //     PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android, ScriptingImplementation.IL2CPP);
+            // else
+            //     PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android, ScriptingImplementation.Mono2x);
+
+            Debug.LogFormat("ApplySetting [Android]\n" +
+                            "PackageName : {0}\n" +
+                            "keyaliasName : {1}, keyaliasPass : {2}\n" +
+                            "keystoreName : {3}, keystorePass : {4}\n" +
+                PlayerSettings.applicationIdentifier,
+                PlayerSettings.Android.keyaliasName, PlayerSettings.Android.keyaliasPass,
+                PlayerSettings.Android.keystoreName, PlayerSettings.Android.keystorePass);
+        }
+
+        public override void OnPostBuild(CLIBuilder builder, ReadOnlyDictionary<string, string> commandLine)
+        {
+        }
+
+        public override string GetBuildPath()
+        {
+            return base.GetBuildPath() + ".apk";
+        }
+    }
+
+    [CustomEditor(typeof(AndroidBuildConfig))]
+    public class AndroidBuildConfig_Inspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            AndroidBuildConfig config = target as AndroidBuildConfig;
+            if (GUILayout.Button("Build!"))
+            {
+                CLIBuilder.Build(config);
+            }
+        }
+    }
+}
--- Assets/unity-cli/Editor/CLIBuilder.cs
+++ Assets/unity-cli/Editor/CLIBuilder.cs
@@ -1,30 +1,14 @@
-#region Header
-
-/*	============================================
- *	개요 : 
- *	에디터 폴더에 있어야 정상 동작합니다.
- *	
- *	참고한 원본 코드 링크
- *	https://slway000.tistory.com/74
- *	https://smilejsu.tistory.com/1528
-   ============================================ */
-
-#endregion Header
-
 using UnityEngine;
 using System;
 using System.IO;
 using System.Linq;
-
-#if UNITY_EDITOR
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.Build.Reporting;
 
 namespace Unity_CLI
 {
     /// <summary>
-    /// 에디터 플레이어 빌드 전 세팅 (빌드 후 되돌리기용)
+    /// 빌드 전 세팅 (빌드 후 되돌리기용)
     /// </summary>
     public class PlayerSetting_Backup
     {
@@ -52,90 +36,7 @@ namespace Unity_CLI
     /// </summary>
     public partial class CLIBuilder
     {
-        public const string const_prefix_EditorContextMenu = "Tools/UNKO/CLI Build/";
-        const string const_prefix_ForLog = "!@#$";
-
-        public enum ECommandLine
-        {
-            /// <summary>
-            /// 결과물이 나오는 파일 명
-            /// </summary>
-            filename,
-
-            /// <summary>
-            /// 컨피그 파일이 있는 절대 경로
-            /// </summary>
-            config_path,
-
-            /// <summary>
-            /// 결과물이 나오는 절대 경로
-            /// </summary>
-            output_path,
-
-            /// <summary>
-            /// PlayerSetting.android.BundleVersionCode
-            /// </summary>
-            android_bundle_versioncode,
-
-            /// <summary>
-            /// PlayerSetting.android.Version
-            /// </summary>
-            android_version,
-
-            /// <summary>
-            /// 스토어에 올라가는 빌드 번호
-            /// </summary>
-            ios_buildnumber,
-
-            /// <summary>
-            /// 스토어에 올라가는 버전
-            /// </summary>
-            ios_version,
-        }
-
-        static readonly IReadOnlyDictionary<ECommandLine, string> commandLine =
-            new Dictionary<ECommandLine, string>()
-            {
-                {ECommandLine.filename, $"{nameof(ECommandLine.filename)}"},
-                {ECommandLine.config_path, $"{nameof(ECommandLine.config_path)}"},
-                {ECommandLine.output_path, $"{nameof(ECommandLine.output_path)}"},
-                {ECommandLine.android_bundle_versioncode, $"{nameof(ECommandLine.android_bundle_versioncode)}"},
-                {ECommandLine.android_version, $"{nameof(ECommandLine.android_version)}"},
-                {ECommandLine.ios_buildnumber, $"{nameof(ECommandLine.ios_buildnumber)}"},
-                {ECommandLine.ios_version, $"{nameof(ECommandLine.ios_version)}"}
-            };
-
-        public static string GetCommandLineString(ECommandLine commandLine) => CLIBuilder.commandLine[commandLine];
-
-        private static BuildConfig s_lastConfig;
-
-
-        [MenuItem(const_prefix_EditorContextMenu + "Create BuildConfig Example Json File")]
-        public static void CreateBuildConfig_DummyJson()
-        {
-            string content = JsonUtility.ToJson(BuildConfig.CreateConfig(), true);
-            string filePath = nameof(BuildConfig) + "_Example.json";
-            File.WriteAllText(Application.dataPath + "/" + filePath, content);
-
-            AssetDatabase.Refresh();
-            UnityEngine.Object jsonFile = AssetDatabase.LoadMainAssetAtPath($"Assets/{filePath}");
-
-            Selection.activeObject = jsonFile;
-            Debug.Log("Create BuildConfig Done", jsonFile);
-        }
-
-        // SO는 아직 고려중
-        //[MenuItem(const_strPrefix_EditorContextMenu + "Create BuildConfig Example SO File")]
-        //public static void Create_BuildConfig_Dummy_SO()
-        //{
-        //    BuildConfig pConfig = BuildConfig.CreateConfig();
-        //    AssetDatabase.CreateAsset(pConfig, $"Assets/{nameof(BuildConfig)} _Example.asset");
-        //    AssetDatabase.SaveAssets();
-        //    AssetDatabase.Refresh();
-
-        //    Selection.activeObject = pConfig;
-        //    Debug.Log("Create BuildConfig Done", pConfig);
-        //}
+        private static BuildConfigBase s_lastConfig;
 
         #region public
 
@@ -148,80 +49,25 @@ namespace Unity_CLI
                     return arguments[i + 1];
             }
 
-            return null;
+            return "";
         }
 
-        public static bool GetFileFromCommandLine<T>(string commandLine, out T outFile)
-            where T : new()
-        {
-            string path = GetCommandLineArg(commandLine);
-            Exception exception = DoTryParsing_JsonFile(path, out outFile);
-            if (exception != null)
-            {
-                Debug.LogErrorFormat(const_prefix_ForLog + " Error - FilePath : {0}, FilePath : {1}\n" +
-                                     " Error : {2}", commandLine, path, exception);
-                return false;
-            }
-
-            return true;
-        }
-
-        public static bool GetFileFromCommandLineSO<T>(string commandLine, out T outFile)
+        public static bool GetSOFromCommandLine<T>(string commandLine, out T outFile)
             where T : ScriptableObject
         {
             string path = GetCommandLineArg(commandLine);
-            Exception exception = DoTryParsing_JsonFileSO(path, out outFile);
+            Exception exception = DoTryParsing_JsonSOFile(path, out outFile);
             if (exception != null)
             {
-                Debug.LogErrorFormat(const_prefix_ForLog + " Error - FilePath : {0}, FilePath : {1}\n" +
-                                " Error : {2}", commandLine, path, exception);
+                Debug.LogErrorFormat("Error - FilePath : {0}, FilePath : {1}\n" +
+                                "Error : {2}", commandLine, path, exception);
                 return false;
             }
 
             return true;
         }
 
-        public static void GetAppFilePath_FromConfig(BuildConfig config, out string buildOutputFolderPath,
-            out string fileName)
-        {
-            string buildOutputFolderPath_CommandLine = GetCommandLineArg(commandLine[ECommandLine.output_path]);
-            string fileName_CommandLine = GetCommandLineArg(commandLine[ECommandLine.filename]);
-
-            buildOutputFolderPath = string.IsNullOrEmpty(buildOutputFolderPath_CommandLine)
-                ? config.buildoutputFolder_absolutepath
-                : buildOutputFolderPath_CommandLine;
-
-            if (string.IsNullOrEmpty(fileName_CommandLine))
-            {
-                fileName = config.filename;
-            }
-            else
-            {
-                fileName = fileName_CommandLine;
-                config.usedatetimesuffix = false;
-            }
-        }
-
-
-        public static Exception DoTryParsing_JsonFile<T>(string jsonFilePath, out T outFile)
-            where T : new()
-        {
-            outFile = default(T);
-
-            try
-            {
-                string configJson = File.ReadAllText(jsonFilePath);
-                outFile = JsonUtility.FromJson<T>(configJson);
-            }
-            catch (Exception exception)
-            {
-                return exception;
-            }
-
-            return null;
-        }
-
-        public static Exception DoTryParsing_JsonFileSO<T>(string jsonFilePath, out T outFile)
+        public static Exception DoTryParsing_JsonSOFile<T>(string jsonFilePath, out T outFile)
             where T : ScriptableObject
         {
             outFile = ScriptableObject.CreateInstance<T>();
@@ -249,43 +95,28 @@ namespace Unity_CLI
         }
 
 
-        public static void Build(BuildConfig buildConfig, string buildOutputFolderAbsolutePath,
-            string fileName, BuildTarget buildTarget)
+        public static void Build(BuildConfigBase buildConfig)
         {
             s_lastConfig = buildConfig;
 
-            OnPreBuild(buildConfig, buildOutputFolderAbsolutePath, fileName, buildTarget, out var buildTargetGroup, out var buildPath);
-
-            BuildPlayerOptions buildPlayerOptions = Generate_BuildPlayerOption(buildConfig, buildTarget, buildPath);
+            BuildTargetGroup buildTargetGroup = BuildPipeline.GetBuildTargetGroup(buildConfig.buildTarget);
+            BuildPlayerOptions buildPlayerOptions = Generate_BuildPlayerOption(buildConfig);
             PlayerSetting_Backup editorSetting_Backup = SettingBuildConfig_To_EditorSetting(buildConfig, buildTargetGroup);
-
-            Debug.LogFormat(const_prefix_ForLog + " Before Build DefineSymbol TargetGroup : {0}\n" +
-                            "Origin Symbol : {1}\n " +
-                            "Config : {2} \n" +
-                            "Current : {3} \n" +
-                            "strBuildPath : {4}",
-                buildTargetGroup,
-                editorSetting_Backup.defineSymbol,
-                buildConfig.definesymbol,
-                PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup),
-                buildPath);
 
             try
             {
-                BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
-                PrintBuildResult(buildPath, report, report.summary);
+                BuildReport report = UnityEditor.BuildPipeline.BuildPlayer(buildPlayerOptions);
+                PrintBuildResult(buildConfig.buildPath, report, report.summary);
             }
             catch (Exception e)
             {
-                Debug.Log(const_prefix_ForLog + " Error - " + e);
+                Debug.Log("Error - " + e);
                 throw;
             }
 
             editorSetting_Backup.Restore();
-            OnPostBuild(buildTarget, buildOutputFolderAbsolutePath);
 
-            Debug.LogFormat(const_prefix_ForLog + " After Build DefineSymbol Current {0}",
-                PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup));
+            Debug.LogFormat("After Build DefineSymbol Current {0}", PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup));
 
             // 2018.4 에서 프로젝트 전체 리임포팅 하는 이슈 대응
             // https://issuetracker.unity3d.com/issues/osx-batchmode-build-hangs-at-refresh-detecting-if-any-assets-need-to-be-imported-or-removed
@@ -294,46 +125,19 @@ namespace Unity_CLI
 #endif
         }
 
-        /// <summary>
-        /// Android를 빌드합니다. CommandLine - ConfigPath 필요
-        /// <para>Command Line으로 실행할 때 partial class file에 있으면 못찾음..</para>
-        /// </summary>
-        public static void Build_Android()
-        {
-            if (GetFileFromCommandLineSO(commandLine[ECommandLine.config_path], out BuildConfig config))
-            {
-                GetAppFilePath_FromConfig(config, out string buildOutputFolderPath, out string fileName);
-                Build(config, buildOutputFolderPath, fileName, BuildTarget.Android);
-            }
-        }
-
-        /// <summary>
-        /// IOS를 빌드합니다. CommandLine - ConfigPath 필요
-        /// <para>Command Line으로 실행할 때 partial class file에 있으면 못찾음..</para>
-        /// </summary>
-        public static void Build_IOS()
-        {
-            if (GetFileFromCommandLineSO(commandLine[ECommandLine.config_path], out BuildConfig config))
-            {
-                GetAppFilePath_FromConfig(config, out string buildOutputFolderPath, out string fileName);
-                Build(config, buildOutputFolderPath, fileName, BuildTarget.iOS);
-            }
-        }
-
         #endregion public
 
         // ==============================================================================================
 
 
         #region private
-        private static BuildPlayerOptions Generate_BuildPlayerOption(BuildConfig buildConfig, BuildTarget buildTarget,
-            string buildPath)
+        private static BuildPlayerOptions Generate_BuildPlayerOption(BuildConfigBase buildConfig)
         {
             BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
             {
-                scenes = buildConfig.buildscenenames.Select(p => p += ".unity").ToArray(),
-                locationPathName = buildPath,
-                target = buildTarget,
+                scenes = buildConfig.buildSceneNames.Select(p => p += ".unity").ToArray(),
+                locationPathName = buildConfig.buildPath,
+                target = buildConfig.buildTarget,
                 options = BuildOptions.None
             };
 
@@ -341,105 +145,21 @@ namespace Unity_CLI
         }
 
 
-        private static PlayerSetting_Backup SettingBuildConfig_To_EditorSetting(BuildConfig buildConfig,
-            BuildTargetGroup buildTargetGroup)
+        private static PlayerSetting_Backup SettingBuildConfig_To_EditorSetting(BuildConfigBase buildConfig, BuildTargetGroup buildTargetGroup)
         {
             string defineSymbol_Backup = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
-            PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, buildConfig.definesymbol);
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, buildConfig.defineSymbol);
 
             string productName_Backup = PlayerSettings.productName;
-            PlayerSettings.productName = buildConfig.productname;
+            PlayerSettings.productName = buildConfig.productName;
 
             return new PlayerSetting_Backup(buildTargetGroup, defineSymbol_Backup, productName_Backup);
         }
 
-        private static void OnPreBuild(BuildConfig buildConfig, string buildOutputFolderAbsolutePath,
-            string fileName, BuildTarget buildTarget, out BuildTargetGroup buildTargetGroup,
-            out string buildPath)
-        {
-            buildTargetGroup = GetBuildTargetGroup(buildTarget);
-            buildPath = CreateBuildPath(buildConfig.usedatetimesuffix, buildOutputFolderAbsolutePath,
-                fileName);
-
-            switch (buildTarget)
-            {
-                case BuildTarget.Android:
-                    BuildSetting_Android(buildConfig.androidSetting);
-                    buildPath += ".apk";
-                    break;
-
-                case BuildTarget.iOS:
-                    BuildSetting_IOS(buildConfig.iOSSetting_);
-                    break;
-            }
-        }
-
-        private static string CreateBuildPath(bool useDateTimeSuffix, string folderName, string fileName)
-        {
-            Debug.LogFormat(const_prefix_ForLog + " FolderName : {0}, FileName : {1}", folderName,
-                fileName);
-
-            try
-            {
-                if (Directory.Exists(folderName) == false)
-                    Directory.CreateDirectory(folderName);
-            }
-            catch (Exception e)
-            {
-                Debug.Log(const_prefix_ForLog + " Error - Create Directory - " + e);
-            }
-
-            string buildPath = folderName + "/" + fileName;
-            if (useDateTimeSuffix)
-            {
-                string strDateTime = DateTime.Now.ToString("MMdd_HHmm");
-                buildPath = buildPath + "_" + strDateTime;
-            }
-
-            return buildPath;
-        }
-
-
-        private static void OnPostBuild(BuildTarget buildTarget, string buildOutputFolderAbsolutePath)
-        {
-            switch (buildTarget)
-            {
-                case BuildTarget.Android:
-
-                    try
-                    {
-                        // Mac OS에서 구동 시 Directory.GetFiles함수는 Error가 나기 때문에
-                        // DirectoryInfo.GetFiles를 통해 체크
-                        DirectoryInfo directory = new DirectoryInfo(buildOutputFolderAbsolutePath);
-                        foreach (var file in directory.GetFiles())
-                        {
-                            // IL2CPP 파일로 빌드 시 자동으로 생inf기는 파일, 삭제해도 무방
-                            if (file.Extension == ".zip" && file.Name.Contains("symbols"))
-                            {
-                                Debug.Log(const_prefix_ForLog + " Delete : " + file.Name);
-                                file.Delete();
-                            }
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        Debug.Log(const_prefix_ForLog + " Error - " + e);
-                    }
-
-                    break;
-            }
-        }
-
-
         private static void PrintBuildResult(string path, BuildReport report, BuildSummary summary)
         {
-            Debug.LogFormat(const_prefix_ForLog + " Path : {0}, Build Result : {1}", path, summary.result);
-
-            if (summary.result == BuildResult.Succeeded)
-            {
-                Debug.Log(const_prefix_ForLog + " Build Succeeded!");
-            }
-            else if (summary.result == BuildResult.Failed)
+            Debug.Log($"Build Result:{summary.result}, Path:{path}");
+            if (summary.result == BuildResult.Failed)
             {
                 int errorIndex = 1;
                 foreach (var step in report.steps)
@@ -448,27 +168,15 @@ namespace Unity_CLI
                     {
                         if (msg.type == LogType.Error || msg.type == LogType.Exception)
                         {
-                            Debug.LogFormat(const_prefix_ForLog + " Build Fail Log[{0}] : type : {1}\n" +
-                                            " content : {2}", ++errorIndex, msg.type, msg.content);
+                            Debug.LogFormat("Build Fail Log[{0}] : type : {1}\n" +
+                                            "content : {2}", ++errorIndex, msg.type, msg.content);
                         }
                     }
                 }
             }
         }
 
-        private static BuildTargetGroup GetBuildTargetGroup(BuildTarget buildTarget)
-        {
-            switch (buildTarget)
-            {
-                case BuildTarget.Android: return BuildTargetGroup.Android;
-                case BuildTarget.iOS: return BuildTargetGroup.iOS;
-            }
-
-            return BuildTargetGroup.Standalone;
-        }
-
         #endregion private
     }
 }
 
-#endif
