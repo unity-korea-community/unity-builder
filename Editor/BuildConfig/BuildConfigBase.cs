eb97ead79b3445cc0c0358a60c7752c288165f0d
KorStrix <KorStrix@users.noreply.github.com>
2021-05-07 16:19:19 +0000

    Update file(s) "Assets/unity-builder/." from "unity-korea-community/unity-builder"

--- Editor/BuildConfig/BuildConfigBase.cs
+++ Editor/BuildConfig/BuildConfigBase.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System;
 
-namespace Unity_CLI
+namespace Unity_Builder
 {
     public interface IBuildConfig
     {
---
+++ Editor/GlobalConst.cs
@@ -0,0 +1,8 @@
+namespace Unity_Builder
+{
+    public static class GlobalConst
+    {
+        public const string CreateAssetMenu_Prefix = "UNKO/unity-builder";
+        public const string EditorContextMenu_Prefix = "Tools/UNKO/unity-builder";
+    }
+}
