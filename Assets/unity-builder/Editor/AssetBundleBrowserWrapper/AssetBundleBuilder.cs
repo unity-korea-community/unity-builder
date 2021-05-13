ef4248de018098387fa3cf3140d85d2588ffd7cb
strix <korstrix@gmail.com>
2021-05-13 20:42:07 +0900

    unity build 다시 커밋

---
+++ Assets/unity-builder/Editor/AssetBundleBrowserWrapper/AssetBundleBuilder.cs
@@ -0,0 +1,55 @@
+using UnityEditor;
+
+namespace Unity_Builder
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AssetBundleBuilder
+    {
+        /* const & readonly declaration             */
+
+
+        /* enum & struct declaration                */
+
+        /* public - Field declaration               */
+
+
+        /* protected & private - Field declaration  */
+
+
+        // ========================================================================== //
+
+        /* public - [Do~Something] Function 	        */
+
+
+        [MenuItem("Tools/Build/BundleBuild Test - Android", priority = 10000)]
+        public static void Build_Android()
+        {
+            AssetBundleBrowserWrapper pWrapper = new AssetBundleBrowserWrapper();
+            pWrapper.BuildBundle(BuildTarget.Android);
+        }
+
+
+        [MenuItem("Tools/Build/BundleBuild Test - IOS", priority = 10000)]
+        public static void Build_IOS()
+        {
+            AssetBundleBrowserWrapper pWrapper = new AssetBundleBrowserWrapper();
+            pWrapper.BuildBundle(BuildTarget.iOS);
+        }
+
+        // ========================================================================== //
+
+        /* protected - [Override & Unity API]       */
+
+
+        /* protected - [abstract & virtual]         */
+
+
+        // ========================================================================== //
+
+        #region Private
+
+        #endregion Private
+    }
+}
--- Assets/unity-builder/sample/com.unko.unity-localize.sample.md
+++
@@ -1,4 +0,0 @@
-# com.unko.unity-localize.sample
-
-{ "name": "com.unko.unity-localize.sample", "references": \[ "GUID:84d9c6112afaf7e46afb8c21b3c22235" \], "includePlatforms": \[\], "excludePlatforms": \[\], "allowUnsafeCode": false, "overrideReferences": false, "precompiledReferences": \[\], "autoReferenced": true, "defineConstraints": \[\], "versionDefines": \[\], "noEngineReferences": false }
-
