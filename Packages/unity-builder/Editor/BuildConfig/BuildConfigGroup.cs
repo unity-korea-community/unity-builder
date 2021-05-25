3ff7f6da1d779d571eaa544be9669a9428f1321e
KorStrix <KorStrix@users.noreply.github.com>
2021-05-25 11:10:11 +0000

    Update file(s) "/." from "unity-korea-community/unity-builder"

--- Packages/unity-builder/Editor/BuildConfig/BuildConfigGroup.cs
+++ Packages/unity-builder/Editor/BuildConfig/BuildConfigGroup.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 namespace Unity_Builder
@@ -8,23 +5,5 @@ namespace Unity_Builder
     [CreateAssetMenu(fileName = "BuildConfigGroup", menuName = GlobalConst.CreateAssetMenu_Prefix + "/BuildConfigGroup")]
     public class BuildConfigGroup : ScriptableObject
     {
-        public List<BuildConfig> configList = new List<BuildConfig>();
-    }
-
-    [CustomEditor(typeof(BuildConfigGroup))]
-    public class BuildConfigGroup_Inspector : Editor
-    {
-        string _commandLine;
-
-        public override void OnInspectorGUI()
-        {
-            base.OnInspectorGUI();
-
-            BuildConfigGroup configGroup = target as BuildConfigGroup;
-            if (GUILayout.Button("All Build!"))
-            {
-                configGroup.configList.ForEach(buildConfig => UnityBuilder.Build(buildConfig));
-            }
-        }
     }
 }
