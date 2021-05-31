6db2849d67d4445e9b281ad30740d23fdc61c35d
KorStrix <KorStrix@users.noreply.github.com>
2021-05-31 13:38:14 +0000

    Update file(s) "Packages/unity-builder/." from "unity-korea-community/unity-builder"

--- Editor/BuildConfig/WindowBuildConfig.cs
+++ Editor/BuildConfig/WindowBuildConfig.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 namespace UNKO.Unity_Builder
 {
     [CreateAssetMenu(fileName = "WindowBuildConfig", menuName = GlobalConst.CreateAssetMenu_Prefix + "/WindowBuildConfig")]
+    [StructLayout(LayoutKind.Auto)] // ignore codacy
     public class WindowBuildConfig : BuildConfig
     {
         public override BuildTarget buildTarget => BuildTarget.StandaloneWindows64;
